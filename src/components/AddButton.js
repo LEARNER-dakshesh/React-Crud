0c250ae60dd93baf769e9098201b88f204e12565
LEARNER-dakshesh <daksheshgupta4@gmail.com>
2024-05-17 23:22:42 +0530

    unorderd_list

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -1,11 +1,34 @@
 import React, { useState } from "react";
 import "./AddButton.css";
 
-const AddButton = ({ insertModuleToggle }) => {
+const AddButton = ({ onModuleCreate }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [moduleName, setModuleName] = useState("");
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
+  };
+
+  const handleCreateModuleClick = () => {
+    setShowModal(true);
+    setIsOpen(false);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    setModuleName("");
+  };
+
+  const handleInputChange = (event) => {
+    console.log("Input changed:", event.target.value);
+    setModuleName(event.target.value);
+  };
+
+  const handleCreate = () => {
+    // console.log("Handle create function called");
+    onModuleCreate(moduleName);
+    closeModal();
   };
 
   return (
@@ -15,16 +38,34 @@ const AddButton = ({ insertModuleToggle }) => {
       </button>
       {isOpen && (
         <div className="dropdown">
-          <div
-            onClick={() => {
-              insertModuleToggle((state) => !state);
-            }}
-            className="dropdown-item"
-          >
+          <div className="dropdown-item" onClick={handleCreateModuleClick}>
             Create module
           </div>
           <div className="dropdown-item">Add a link</div>
           <div className="dropdown-item">Upload</div>
+        </div>
+      )}
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <h1>Create new Module</h1>
+            <h2>Module name</h2>
+            <input
+              type="text"
+              value={moduleName}
+              onChange={handleInputChange}
+              placeholder="Enter module name"
+              className="module-input"
+            />
+            <div className="modal-actions">
+              <button className="cancel-button" onClick={closeModal}>
+                Cancel
+              </button>
+              <button className="create-button" onClick={handleCreate}>
+                Create
+              </button>
+            </div>
+          </div>
         </div>
       )}
     </div>
--- src/components/Coursebuilder.js
+++ src/components/Coursebuilder.js
@@ -1,26 +1,61 @@
+// import React, { useState } from "react";
+// import AddButton from "./AddButton";
+// import "./CourseBuilder.css";
+// import img1 from "../images/img1.png";
+
+// const CourseBuilder = () => {
+
+//   return (
+//     <div className="course-builder">
+//       {" "}
+//       <header>
+//         <h1>Course builder</h1>
+//         <AddButton />{" "}
+//       </header>{" "}
+//       <div className="content">
+//         {" "}
+//         <div className="empty-state">
+//           <img src={img1} alt="Box with items" />{" "}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default CourseBuilder;
+
 import React, { useState } from "react";
 import AddButton from "./AddButton";
 import "./CourseBuilder.css";
 import img1 from "../images/img1.png";
 
 const CourseBuilder = () => {
-  const [insertModuleModal, setInsertModuleModal] = useState(false);
+  const [modules, setModules] = useState([]);
+
+  // Function to add a new module to the list
+  const addModule = (moduleName) => {
+    console.log("Moduledss:", modules);
+    setModules([...modules, moduleName]);
+  };
 
   return (
     <div className="course-builder">
-      {insertModuleModal ? (
-        <div id="InsertModuleModal" className="insertModal">
-          Hello this is modal
-        </div>
-      ) : null}
       <header>
         <h1>Course builder</h1>
-        <AddButton insertModuleToggle={setInsertModuleModal} />
+        <AddButton onModuleCreate={addModule} />{" "}
       </header>
       <div className="content">
-        <div className="empty-state">
-          <img src={img1} alt="Box with items" />
-        </div>
+        {modules.length === 0 ? (
+          <div className="empty-state">
+            <img src={img1} alt="Box with items" />
+          </div>
+        ) : (
+          <ul className="module-list">
+            {modules.map((module, index) => (
+              <li key={index}>{module}</li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
